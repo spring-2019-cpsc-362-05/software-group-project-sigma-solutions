3e7356877832e9e5641ad98867c333adb9e4f289
Brandon Lavello <brandonlavello@csu.fullerton.edu>
2019-05-06 05:38:43 -0700

    Updated main

--- Cardpp/Main.cpp
+++ Cardpp/Main.cpp
@@ -1,25 +1,28 @@
 ﻿#include "StaticVars.h"
 #include "Card.h"
 #include "Game.h"
+#include "mainmenu.h"
 
 #include <QStateMachine>
 #include <QEventTransition>
 #include <QPropertyAnimation>
 
-Game* game;
-
+//Game* game;
 int main(int argc, char* argv[]) {
 
     QApplication a(argc, argv);
-    game = new Game();
-    int numDecks = 4,
-        active = 127,
-        control = 85,
-        strategy = 1;
-    //Brandon's Menu Stuff
-    game->show();
-    game->start(numDecks, active, control, strategy);
-    game->run();
+    mainMenu * menu = new mainMenu();
+    menu->show();
+
+//    game = new Game();
+//    int numDecks = 4,
+//        active = 127,
+//        control = 85,
+//        strategy = 1;
+//    //Brandon's Menu Stuff
+//    game->show();
+//    game->start(numDecks, active, control, strategy);
+//    game->run();
 
     return a.exec();
 }
